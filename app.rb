0a41b98742dab4b1f13546427cfdda3cf0e61524
Vitor Fernandes <vmlf01@gmail.com>
2015-11-05 00:32:07 +0000

    prevent fake win or lose

--- app.rb
+++ app.rb
@@ -69,12 +69,20 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      erb :show
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      erb :show
+    end
   end
   
 end
