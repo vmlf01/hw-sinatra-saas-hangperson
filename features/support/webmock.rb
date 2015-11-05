371ce58086acd3a4ccee679cbca1a156be4db191
Vitor Fernandes <vmlf01@gmail.com>
2015-11-05 20:47:52 +0000

    fix webmock setting to allow codacy/http connection

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,5 +1,7 @@
 require 'webmock/cucumber'
 
+WebMock.allow_net_connect!(:net_http_connect_on_start => true)
+
 Before do
   stub_request(:post, "http://watchout4snakes.com/wo4snakes/Random/RandomWord").to_return(:status => 200, :headers => {}, :body => "testword")
 end
